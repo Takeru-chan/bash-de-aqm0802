e67c07702d9b97c5c0631c63ffdfb82712a764ed
Takeru-chan <takeru.katayama@gmail.com>
2016-03-27 16:26:41 +0900

    first commit.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# bash-de-aqm0802
+# Shell script to display string on AQM0802
+会社帰りに千石電商で800円ほどの小さな液晶を買いました。Raspberry PiとI<sup>2</sup>Cで繋がるタイプのもので、裏側にAQM0802A-RN-GBWと印字されていました。  
+![AQM8020](./images/aqm8020.jpg)  
+[スイッチサイエンスのブログ](http://mag.switch-science.com/2013/07/30/use-i2c-lcd-brakeout-board-on-raspberry-pi/)に載っていた使い方を参考に書いたシェルスクリプトです。  
+
+## Usage | 使い方
+ターミナルでスクリプトを実行します。  
+- 引数なし  
+ 入力プロンプトに指定した内容を液晶に表示します。  
+- 引数あり  
+ 指定された引数の内容を液晶に表示します。  
+
+液晶の仕様が８桁２行なので、これを超えた分は表示されません。スクロール処理などはありません。  
+
+- 動作確認機種：RASPBERRY PI 2 MODEL B  
+- 動作確認OS：raspbian-jessie-lite / Firmware 4.1.xx  
+- 使用ツール：i2c-tools  
+- 対象モジュール：AQM0802  
+
+## Known issue | 既知の問題
+なぜか!が連続すると正しい文字列を表示しません。  
+
+## License
+This script has released under the MIT license.  
+[http://opensource.org/licenses/MIT](http://opensource.org/licenses/MIT)  
+
+## One more thing...
+日付と時刻を表示させたければハードリンクでgettimeという名前にして実行してください。自動的に１行目に年月日、２行目に時分と曜日を表示します。  
+cronで毎分実行させるとデジタル時計のできあがり。  
+![デジタル時計Pi](./images/raspberrypi-with-aqm0802.jpg)  
---
+++ disp.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+if test "`uname`" != "Linux"; then
+    echo "This system is not supported."
+    exit
+fi
+for index in `seq 0 $((${#0} -1))`; do
+    if test "${0:$index:1}" == "/"; then
+        end_path=$index
+    fi
+done
+gettime=0
+if test "${0:$(($end_path + 1))}" == "gettime"; then
+    gettime=1
+fi
+detect=`sudo i2cdetect -y 1 | grep "3e" | wc -l`
+if test $detect -eq 0; then
+    if test $gettime -eq 0; then
+        echo "AQM0802 is not installed."
+    fi
+    exit
+fi
+
+device="0x3e"
+send_command="0x00"
+send_data="0x40"
+
+init_first="0x38 0x39 0x14 0x70 0x56 0x6c i"
+init_second="0x38 0x0d 0x01 i"
+
+position_00="0x02 i"
+position_10="0xc0 i"
+
+function hexcode() {
+    code=""
+    if test ${#strings} -ne 0; then
+        for index in `seq 0 $((${#strings} - 1))`; do
+            code_dec=`printf "%d" \'${strings:$index:1}`
+            code_hex=`printf "%x" $code_dec`
+            code="$code 0x$code_hex"
+        done
+    fi
+}
+
+sudo i2cset -y 1 $device $send_command $init_first
+sudo i2cset -y 1 $device $send_command $init_second
+if test "$1" != ""; then
+    strings=$1
+else
+    if test $gettime -eq 0; then
+        echo -n "Input first line > "
+        read strings
+    else
+        strings=`date +%Y%m%d`
+    fi
+fi
+hexcode
+sudo i2cset -y 1 $device $send_data $code i
+sudo i2cset -y 1 $device $send_command $position_10
+if test "$2" != ""; then
+    strings=$2
+else
+    if test $gettime -eq 0; then
+        echo -n "Input second line > "
+        read strings
+    else
+        strings=`date +%H:%M_%a`
+    fi
+fi
+hexcode
+sudo i2cset -y 1 $device $send_data $code i
